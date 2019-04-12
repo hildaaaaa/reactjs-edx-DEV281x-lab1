010cc5bd561e3e1b0fbd81001afdc33dcb05956f
hilda <ylchungac@connect.ust.hk>
2019-04-12 23:28:51 +0800

    init and render html with reactjs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function TitleDiv(props) {
+	return (
+		<h2>{props.welcome}</h2>
+		)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function OptionsDiv(props) {
+	return (
+		<div>
+			<h2></h2>
+		</div>
+		)
+}
+
+function RowData(props) {
+  return (
+    <tr>
+      <td>{props.rowData[0]}</td>
+      <td>{props.rowData[1]}</td>
+      <td>${props.rowData[2]}</td>
+      <td>
+        <button>Buy Now</button>
+      </td>
+    </tr>
+  )
+}
+
+function RowHeader(props) {
+  return (
+    <tr>
+      <th>Year</th>
+      <th>Model</th>
+      <th>Price</th>
+      <th>Buy</th>
+    </tr>
+  );
+}
+
+function YearRowTable(props) {
+  return (
+    <ul>
+      <table>
+          <RowHeader />
+          <RowData rowData={props.yrData}/>
+      </table>
+    </ul>
+  );
+}
+
+function TransportTable(props) {
+  return (
+    <div>
+      	<h2>{props.title}</h2>
+      	{props.tableData.map((obj, i) => <YearRowTable yrData={obj}/>)}
+    </div>
+  );
+}
+
+function TransportationsList(props) {
+  const carTable = [
+    [ "2013","A","32000" ],
+    [ "2011","B","4400" ],
+    [ "2016","B","15500" ]
+  ];
+  const truckTable = [
+    [ "2014","D","18000"],
+    [ "2013","E","5200"]
+  ];
+  const conTable = [
+    [ "2009","F","2000"],
+    [ "2010","G","6000"],
+    [ "2012","H","12500"],
+    [ "2017","M","50000"]
+  ];
+
+  return (
+  	<div>
+  		<TransportTable title="Cars" tableData={carTable}/>
+    	<TransportTable title="Trucks" tableData={truckTable}/>
+    	<TransportTable title="Convertibles" tableData={conTable}/>
+  	</div>
+  	);
+}
+
+ReactDOM.render(
+  <TransportationsList />,
+  document.getElementById("tbl"),
+);
+
